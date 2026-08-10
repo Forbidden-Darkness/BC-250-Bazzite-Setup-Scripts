449b87416be4f0f41ae84a1daeaefe43376df4f4
Forbidden Darkness <ny.forbiddendarkness@gmail.com>
2026-08-10 14:38:16 -0700

    Update shortcut properties for Bazzite Toolbox

--- start.sh
+++ start.sh
@@ -265,8 +265,9 @@ ask_desktop_shortcut() {
 [Desktop Entry]
 Type=Application
 Name=Bazzite Boken Toolbox
-Comment=Manage Memory - Overclock - Wake on Lan
-Exec=konsole -e sudo bash "$SCRIPT_PATH"
+Comment=Launch Custom Bazzite Tweak Tool
+# Pushes explicit character cell boundaries to Konsole 
+Exec=konsole --geometry 130x38 -e sudo bash "$SCRIPT_PATH"
 Icon=utilities-terminal
 Terminal=false
 Categories=System;
